8af512aa5191a2ebead5c681911b77fd1629f06a
Ralf Kraemer <100492510+Ralf-Kraemer@users.noreply.github.com>
2026-01-13 02:54:03 +0100

    dependency refactoring

--- lib/toot_ui.dart
+++ lib/toot_ui.dart
@@ -9,7 +9,7 @@ export 'helper.dart';
 export 'default_text_styles.dart';
 export 'src/compact_toot_view.dart';
 export 'src/embedded_toot_view.dart';
-export 'src/toot_view.dart';
+export 'models/viewmodels/toot_view.dart';
 export 'src/inline_reply_form.dart';
 export 'src/status_form.dart';
 export 'models/api/v1/mastodonstatus.dart';
